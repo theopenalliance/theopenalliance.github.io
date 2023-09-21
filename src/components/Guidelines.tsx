c3412fe66805b119c9512336727824cb5180d926
Justin Tervay <justin@tervay.com>
2023-09-21 16:34:01 -0400

    update join page

--- src/components/Guidelines.tsx
+++ src/components/Guidelines.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Guidelines(): JSX.Element {
   const guidelines: (JSX.Element | String)[] = [
     "Open Alliance teams don't keep secrets.",
-    "Open Alliance teams do their best to share updates at least once a week.",
-    "Open Alliance teams primarily post updates to ChiefDelphi.",
+    "Open Alliance teams do their best to share updates at least once a week during the competition season and once a month during the off-season.",
+    "Open Alliance teams primarily post updates to ChiefDelphi and are also encouraged to post to the OA Discord and other mediums.",
     "Open Alliance teams provide open CAD and/or open code.",
     "Open Alliance teams include the bad with the good.",
     "Open Alliance teams join to help everyone - not just other Open Alliance teams.",
--- src/pages/join.tsx
+++ src/pages/join.tsx
@@ -15,36 +15,17 @@ export default function Join(props: {}): JSX.Element {
         <br />
         <h1>Joining The Open Alliance</h1>
         <p>
-          Anyone is welcome to post content on ChiefDelphi with an{" "}
-          <a href="https://www.chiefdelphi.com/tag/openalliance">
-            <code>#openalliance</code> tag
-          </a>
-          . If your team will be posting regular updates, we encourage you to
-          apply to The Open Alliance via the following form:
+          We encourage your team to register to be officially recognized as an
+          Open Alliance team. Post content utilizing #openalliance and register
+          via the following form:
         </p>
         <p>
-          <a href="https://forms.gle/jXfSQ7SaVJ7S29b48">
-            https://forms.gle/jXfSQ7SaVJ7S29b48
+          <a href="https://forms.gle/JkJqjR3oL8oG6m1r8">
+            https://forms.gle/JkJqjR3oL8oG6m1r8
           </a>
         </p>
-        <p>
-          Once your team has posted 2 updates on your ChiefDelphi build thread,
-          we will add your team to the Open Alliance website. Feel free to
-          message{" "}
-          <a href="https://www.chiefdelphi.com/u/jtrv/summary">Justin</a> on
-          Chief if there is a delay in this.
-        </p>
-        <p></p>
         <p>
           <h1>Guidelines</h1>
-          Open Alliance teams are expected to provide the following resources:
-          <ul>
-            <li>
-              ChiefDelphi build thread, with regular updates (once a week
-              recommended)
-            </li>
-            <li>Either public code OR public CAD</li>
-          </ul>
           <Guidelines />
         </p>
       </Container>
